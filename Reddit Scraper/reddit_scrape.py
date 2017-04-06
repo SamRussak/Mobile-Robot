436f4bcd48779e7b1ad7a699170bf27ae699652e
Sam Russak <sjr59@pitt.edu>
2017-04-06 10:03:49 -0400

    add subreddit to command line input

--- Reddit Scraper/reddit_scrape.py
+++ Reddit Scraper/reddit_scrape.py
@@ -4,9 +4,10 @@ import csv
 import praw
 import string
 
-
+userInput = sys.argv
+sub_name = userInput[1]
 reddit = praw.Reddit('bot1')
-sub = reddit.subreddit("politics")
+sub = reddit.subreddit(sub_name)
 
 for entries in sub.hot(limit=5):
 	print("Title: ", entries.title)
