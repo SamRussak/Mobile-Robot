f20b67c3c67fcc1b7489bb0853afec831d8c78da
Sam Russak <sjr59@pitt.edu>
2017-05-25 12:04:41 -0400

    show worldnews/news top/rising

--- Reddit Scraper/reddit_scrape.py
+++ Reddit Scraper/reddit_scrape.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import sys
 import csv
@@ -23,23 +22,3 @@ sub = reddit.subreddit("worldnews")
 pri(sub)
 sub = reddit.subreddit("news")
 pri(sub)
-=======
-import pandas as pd
-import sys
-import csv
-import praw
-import string
-
-userInput = sys.argv
-sub_name = userInput[1]
-reddit = praw.Reddit('bot1')
-sub = reddit.subreddit(sub_name)
-
-for entries in sub.hot(limit=5):
-	print("Title: ", entries.title)
-	print("Text: ", entries.url)
-	print("Score: ", entries.score)
-	print("---------------------------------\n")
-
- 
->>>>>>> origin/master
