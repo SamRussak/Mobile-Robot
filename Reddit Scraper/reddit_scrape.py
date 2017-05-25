1fd55b332be0a91b22dfb9ea6257d65fdcd69c6d
Sam Russak <sjr59@pitt.edu>
2017-05-25 12:02:27 -0400

    Show top and rising news/worldnews

--- Reddit Scraper/reddit_scrape.py
+++ Reddit Scraper/reddit_scrape.py
@@ -5,13 +5,20 @@ import praw
 import string
 
 
+def pri(sub):
+	print("Hot:")
+	for entries in sub.hot(limit=5):
+		print("Title: ", entries.title)
+		print("---------------------------------\n")
+
+	print("Rising:")
+	for entries in sub.rising(limit=5):
+		print("Title: ", entries.title)
+		print("---------------------------------\n")
+
+
 reddit = praw.Reddit('bot1')
-sub = reddit.subreddit("politics")
-
-for entries in sub.hot(limit=5):
-	print("Title: ", entries.title)
-	print("Text: ", entries.url)
-	print("Score: ", entries.score)
-	print("---------------------------------\n")
-
- 
+sub = reddit.subreddit("worldnews")
+pri(sub)
+sub = reddit.subreddit("news")
+pri(sub)
